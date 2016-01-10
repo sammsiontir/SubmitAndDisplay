7312abe3f904440855db5c16a4178956b39b68d2
sammsiontir <chengfu@umich.edu>
2016-01-09 19:45:11 -0800

    Create new android app project

---
+++ app/src/androidTest/java/com/example/chengfu_lin/submitanddisplay/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.chengfu_lin.submitanddisplay;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
